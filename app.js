64f593d3e5a78eeae7e8fac761fe4d159f04651f
ChloeArd <chloe.ardoise@gmail.com>
2020-12-01 18:11:12 +0100

    Exercice 66

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ let entier1 = parseInt("32");
 let result = entier1 * operande;
 entier.innerHTML = result;
 
-
 let flottant = document.getElementById('flottant');
 let flottant1 = parseFloat("54.85");
 let result2 = (flottant1 * operande) + result;
 flottant.innerHTML = result2;
 
 let nonNumerique = document.getElementById('non-numerique');
+let num = "Vous y arriverez !";
+let result3;
 
+if (isNaN(num)) {
+    num = 0;
+    nonNumerique.innerHTML = num;
+}
+
+else {
+    result3 = parseInt(num);
+    nonNumerique.innerHTML = num;
+}
 
 let random1 = document.getElementById('random');
 let aleatoire;
@@ -30,8 +40,6 @@ let replace = document.getElementById('replace');
 let world = "Hello world !";
 let name = world.replace("world", "Ardoise Chloé");
 replace.innerHTML = name;
-
-
 
 
 console.log(random());
