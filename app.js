cdce6eddb42e139144e8e303c032800a402ce2c3
ChloeArd <chloe.ardoise@gmail.com>
2020-12-01 18:00:55 +0100

    Exercice 66

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+let operande = 10;
+
+let entier = document.getElementById('entier');
+let entier1 = parseInt("32");
+let result = entier1 * operande;
+entier.innerHTML = result;
+
+
+let flottant = document.getElementById('flottant');
+let flottant1 = parseFloat("54.85");
+let result2 = (flottant1 * operande) + result;
+flottant.innerHTML = result2;
+
+let nonNumerique = document.getElementById('non-numerique');
+
+
+let random1 = document.getElementById('random');
+let aleatoire;
+for( let i = 0; i < 1; i++) {
+    aleatoire = Math.trunc(Math.random() * 100);
+    random1.innerHTML = aleatoire;
+}
+
+let trunc1 = document.getElementById('trunc');
+let trunc2 = 54.988;
+let result5 = Math.trunc(trunc2);
+trunc1.innerHTML = result5;
+
+let replace = document.getElementById('replace');
+let world = "Hello world !";
+let name = world.replace("world", "Ardoise Chloé");
+replace.innerHTML = name;
+
+
+
+
 console.log(random());
